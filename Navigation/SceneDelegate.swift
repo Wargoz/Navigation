6d870c1d3ed562c8c58b69845a30f7728741ae37
Wargoz <lawyer.varganov@gmail.com>
2022-11-01 01:08:11 +0500

    add tabController

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -15,16 +15,47 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let feedViewController = UINavigationController (rootViewController: FeedViewController ())
-        let profileViewContriller = UINavigationController (rootViewController: ProfileViewController ())
         
-        let tabBarController = UITabBarController ()
-        tabBarController.viewControllers = [feedViewController, profileViewContriller]
+    
+      
+        let viewControllers = [createTabBarController(), createProfileViewController()]
         
         window = UIWindow (frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = tabBarController
+        window?.rootViewController = createTabBarController()
         window?.makeKeyAndVisible()
+        
+        func createFeedViewController () -> UINavigationController {
+            
+            let feedViewController = FeedViewController ()
+            feedViewController.title = "Лента"
+            feedViewController.tabBarItem = UITabBarItem(title: "Лента", image: UIImage (systemName: "doc.richtext"), tag: 0)
+            
+            return UINavigationController(rootViewController: feedViewController)
+            
+        }
+        
+        func createProfileViewController () -> UINavigationController {
+            
+            let profileViewController = ProfileViewController ()
+            profileViewController.title = "Профиль"
+            profileViewController.tabBarItem = UITabBarItem (title: "Профиль", image: UIImage(systemName: "person.circle"),tag: 1)
+            
+            return UINavigationController (rootViewController: profileViewController)
+            
+        }
+        
+        func createTabBarController () -> UITabBarController {
+            
+            let tabBarController = UITabBarController ()
+            UITabBar.appearance().backgroundColor = .systemBlue
+            tabBarController.viewControllers = [createFeedViewController(), createProfileViewController()]
+            
+            return tabBarController
+
+        }
+        
+        
         
     }
 
